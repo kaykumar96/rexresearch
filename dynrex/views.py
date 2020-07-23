fa04830b895fc1cb5d6501cf9ab9e33e8f6ec611
Supraja ch <supraja@accusaga.com>
2020-07-23 12:58:30 +0530

    changed files

--- dynrex/views.py
+++ dynrex/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from dynrex.models import Content, ContentDetails
+from dynrex.contentSerializers import ContentSerializer, ContentDetailsSerializer
+from django.db.models import Q
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import  TokenAuthentication
 # Create your views here.
+
+class contentlist(generics.ListCreateAPIView):
+	queryset = Content.objects.filter(status=0).all()
+	serializer_class = ContentSerializer
+	# permission_classes = [IsAuthenticated,]
+	# authentication_classes = [TokenAuthentication]
+
+
+	def get_queryset(self):
+		""" allow rest api to filter by submissions """
+		queryset     = Content.objects.all()
+		return queryset
+
+class contentshow(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Content.objects.all()
+	serializer_class = ContentSerializer
+	# permission_classes = [IsAuthenticated,]
+	# authentication_classes = [TokenAuthentication]
+
+class contentdetailslist(generics.ListCreateAPIView):
+	queryset = ContentDetails.objects.all()
+	serializer_class = ContentDetailsSerializer
+	# permission_classes = [IsAuthenticated,]
+	# authentication_classes = [TokenAuthentication]
+
+
+	def get_queryset(self):
+		""" allow rest api to filter by submissions """
+		queryset     = ContentDetails.objects.select_related('content').all()
+		return queryset
+
+class contentdetailsshow(generics.RetrieveUpdateDestroyAPIView):
+	queryset = ContentDetails.objects.select_related('content').all()
+	serializer_class = ContentDetailsSerializer
+	# permission_classes = [IsAuthenticated,]
+	# authentication_classes = [TokenAuthentication]
